b0526da28f51ddd5cef198275e0fc5466e35b115
7eritori8 <7eritori8@gmail.com>
2023-03-11 18:01:15 +0900

    計算機作りました

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let Yen = 0;
+
+    const Dollar = 0.0074;
+    const EUR = 0.007;
+    const Yuan = 0.051;
+    function getYen(e) {
+        let value = e.target.value;
+        if (value !== null && value >= 0) {
+            Yen = value;
+        }
+    }
+</script>
+
+<h1>円→通貨計算機</h1>
+<input on:input={getYen} type="number" min="0" maxlength="10" value="0" /><span
+    >円</span
+>
+{#if Yen !== null}
+    <h2>{Yen}円は・・・</h2>
+    <p>{Dollar * Yen}ドル</p>
+    <p>{EUR * Yen}ユーロ</p>
+    <p>{Yuan * Yen}元</p>
+{/if}
---
+++ src/routes/tutorial/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+    console.log("aaa");
+    let name = "eri";
+    let src = "https://placehold.jp/150x150.png";
+    import Child from "./child.svelte";
+    let count = 0;
+    function plusCount() {
+        count = count + 1;
+    }
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<p>{name}</p>
+<input type="text" />
+
+<!-- 画像 -->
+<img {src} alt="" srcset="" />
+<!-- 変数名と属性名が同じならこれでもOK -->
+<img {src} alt="" srcset="" />
+
+<!-- 別コンポーネントをインポート、表示 -->
+<Child />
+
+<button on:click={plusCount}> ぷっしゅ </button>
+<span>{count}</span>
+
+<!-- 別ページを作るときは、routesディレクトリ直下に作りたいパス名のフォルダを作成 -->
+<!-- 作成したフォルダの中に+page.svelteファイルを作ると表示される -->
+
+<!-- routesディレクトリ直下はホーム -->
+<style>
+    p {
+        color: red;
+    }
+</style>
